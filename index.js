8b11232da88c5ab7dcbec011bbcff89e607d67fd
timotheemcdonald <timotheemcdonald@gmail.com>
2020-09-19 13:32:03 -0400

    fixing deployment error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require('./api/server')
 
 const PORT = process.env.PORT || 7000
+
 server.listen(PORT, () => {
     console.log(`\n** === Server Listening on Port ${PORT} === **\n`)
 })
